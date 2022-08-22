c0affd4bd0ff8f4a0312881f050f0d5b78aac1de
BeTetiana <tibedina@gmail.com>
2022-08-22 19:22:18 +0300

    addede dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyWord} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function updateKeyWord(event) {
     setKeyWord(event.target.value);
   }
