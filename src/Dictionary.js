8a3bc8b1dc98d54004ecd15f029c8cf68a07da43
BeTetiana <tibedina@gmail.com>
2022-08-24 13:28:52 +0300

    changed the look

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -21,19 +21,23 @@ export default function Dictionary() {
     setKeyWord(event.target.value);
   }
   return (
-    <div className="Dictionary shadow">
-      <h2>What word do you want to look up?</h2>
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Search for a word..."
-          autofocus="true"
-          onChange={updateKeyWord}
-        />
-        <input type="submit" value="Search" />
-      </form>
-      <h4>Get definitions, synonyms, translations and more</h4>
-      <Results results={results} />
+    <div className="Dictionary">
+      <div className="Dictionary-box shadow">
+        <h2>What word do you want to look up?</h2>
+        <form onSubmit={search}>
+          <input
+            type="search"
+            placeholder="Search for a word..."
+            autofocus="true"
+            onChange={updateKeyWord}
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <h4>Get definitions, synonyms, translations and more</h4>
+      </div>
+      <div className="results">
+        <Results results={results} />
+      </div>
     </div>
   );
 }
